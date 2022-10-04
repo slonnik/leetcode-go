b1a615b333554b2dfc0073400820b2c462f7b1b6
ruaabsm <aleksey.y.palamarchuk@raiffeisen.ru>
2022-10-04 10:48:37 +0300

    refactor

--- problem226/problem226.go
+++ problem226/problem226.go
@@ -6,5 +6,14 @@ https://leetcode.com/problems/invert-binary-tree/
 import "github.com/slonnik/leetcode-go/util"
 
 func InvertTree(root *util.TreeNode) *util.TreeNode {
-	return nil
+	if root == nil {
+		return root
+	}
+
+	tempNode := root.Right
+	root.Right = root.Left
+	root.Left = tempNode
+	InvertTree(root.Left)
+	InvertTree(root.Right)
+	return root
 }
--- problem226/problem226_test.go
+++ problem226/problem226_test.go
@@ -13,3 +13,11 @@ func TestInvertTrivial(t *testing.T) {
 		t.Fatalf("Failed to invert: %v", result)
 	}
 }
+
+func TestInvertNonTrivial(t *testing.T) {
+	node := util.TreeNodeFromSlice([]int{4, 2, 7, 1, 3, 6, 9})
+	result := InvertTree(node).ToSlice()
+	if !reflect.DeepEqual(result, []int{4, 7, 2, 9, 6, 3, 1}) {
+		t.Fatalf("Failed to invert: %v", result)
+	}
+}
